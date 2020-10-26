499b1af322de5df555ff6db24c5d13bfc66d4aa3
dorianmeade <dorianmeade@ufl.edu>
2020-10-26 07:28:44 -0400

    changed uri and added proxy

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks'
 import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000' //!will change in production
+    uri: 'https://shrouded-dawn-76328.herokuapp.com/'
 })
 
 const authLink = setContext(() => {
